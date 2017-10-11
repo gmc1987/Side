19fb890fbda31f91b9be6dd0beb0fd87a17692a0
gmc870223 <gmc870223@localhost>
2017-10-11 23:13:46 +0800

    派号功能jsp内容

--- TSSA/WebContent/WEB-INF/view/AllocateSeatsManage/AllocateSeatsNumberRecordManage.jsp
+++ TSSA/WebContent/WEB-INF/view/AllocateSeatsManage/AllocateSeatsNumberRecordManage.jsp
@@ -3,21 +3,95 @@
 <c:set var="pageNocache" value="true"></c:set>
 <c:set var="pageTitle" value="时代折扣移动支付平台"></c:set>
 <c:set var="pageExt" value="true"></c:set>
-<c:set var="pageMode" value="${pageMode}"></c:set>
+<c:set var="pageMode" value="${result}"></c:set>
 <c:set var="pageAjax" value="true"></c:set> 
 <c:set var="title" value="系统派位管理" />
 <%@ include file="../../../js/js.inc" %> 
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="zh-CN">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>${title}</title>
-<script type="text/javascript" src="${ctx}/js/side/AllocateSeatsNumberRecordManage/AllocateSeatsNumberRecordManage.js"></script>
-<script type="text/javascript">
-var ctx = '${ctx}';
-</script>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>${title}</title>
+	<script type="text/javascript">
+	var ctx = '${ctx}';
+	</script>
+	<script type="text/javascript" src="${ctx}/js/common/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="${ctx}/js/common/bootstrap.min.js"></script>
+	<script type="text/javascript" src="${ctx}/js/side/AllocateSeatsNumberRecordManage/AllocateSeatsNumberRecordManage.js"></script>
+	<link href="${ctx}/css/strap/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-	<div id="allocateNumber"></div>
+	<div class="row" style="padding-left: 10px;padding-top: 10px;">
+	<c:forEach var="list" items="${pageMode}">
+		<div class="col-sm-3">
+			<div class="panel panel-primary">
+				<div class="panel-heading">${list.typeName}</div>
+				<div class="panel-body">
+					<table class="table">
+						<tr>
+							<td>
+								<label class="label label-info">当前号数:</label>
+							</td>
+							<td>
+								<span id="num_${list.typeId}" style="font-size: 11px;">${list.num}</span>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<label class="label label-info">取号客户:</label>
+							</td>
+							<td>
+								<span id="custNo_${list.typeId}" style="font-size: 11px;">${list.custNo}</span>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<label class="label label-info">生成时间:</label>
+							</td>
+							<td>
+								<span id="createDate_${list.typeId}" style="font-size: 11px;">${list.createDate}</span>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<label class="label label-info">当前状态:</label>
+							</td>
+							<td>
+								<c:choose>
+									<c:when test="${list.status==0}">
+										<span id="status_${list.typeId}" style="font-size: 11px;">${list.status}:可取号</span>
+									</c:when>
+									<c:when test="${list.status==1}">
+										<span id="status_${list.typeId}" style="font-size: 11px;">${list.status}:已取号</span>
+									</c:when>
+									<c:otherwise>
+										<span id="status_${list.typeId}" style="font-size: 11px;">${list.status}:已过号</span>
+									</c:otherwise>
+								</c:choose>
+								<input type="hidden" name="typeId" value="${list.typeId }">
+							</td>
+						</tr>
+						<tr>
+							<td colspan="2" align="center">
+								<div class="btn-group btn-group-xs">
+									<button id="btn_${list.numUuid}" class="btn btn-default" onclick="invalidNum('${list.numUuid}', '${list.num}', this)">作废</button>
+									<button id="btn_${list.typeId}" class="btn btn-default" onclick="createNewNum('${list.typeId}','${list.numUuid}')">出号</button>
+								</div>
+							</td>
+						</tr>
+					</table>
+					<p id="remark_${list.typeId}" style="align-content: center; font-size: 11px; color: red;">
+						${list.remark}
+					</p>
+				</div>
+			</div>
+		</div>
+	</c:forEach>
+	</div>
+	<div class="alert alert-success" style="display: none;"></div>
+	<div class="alert alert-danger" style="display: none;"></div>
 </body>
 </html>
--- TSSA/WebContent/js/side/AllocateSeatsNumberRecordManage/AllocateSeatsNumberRecordManage.js
+++ TSSA/WebContent/js/side/AllocateSeatsNumberRecordManage/AllocateSeatsNumberRecordManage.js
@@ -1,48 +1,132 @@
-Ext.onReady(function(){
-	
-	var viewPanel = new Ext.Panel({
-		id : "mainPanel",
-//		title : "出号管理",
-		collapsible : "true",
-		autoScroll : "true",
-		layout : "table",
-		renderTo : "allocateNumber",
-		width : "100%",
-		height : "100%"
-	});
-	
-	//获取后台商户设置的派位类型数据
-	Ext.Ajax.request({
-		url : ctx + '/allocateSeats/getList.do',
-		method : 'POST',
-		success : function(response,request){
-			var jsonObj = eval('(' + response.responseText + ')');
-			pageInit(jsonObj.records);
+var timer = null;
+
+/**
+ * 号码作废方法
+ * @param uuid
+ * @param num
+ * @returns
+ */
+function invalidNum(uuid, num, btn){
+	$(btn).attr("disabled", "disabled");
+	var newUuid = "";
+	var newNum = "";
+	$.ajax({
+		url : ctx + "/allocateSeatNumberRecord/numInvalid.do",
+		data : {
+			uuid:uuid,
+			num:num
 		},
-		failure : function(response,request){
-				Ext.MessageBox.hide();
-				Ext.MessageBox.alert("提示","删除失败");
+		type : "POST",
+		dataType : "json",
+		success : function(data){
+			if(data.success == true){
+				$("#num_" + data.data.allocateSeatType.uuid).html(data.data.allocateNo);
+				$("#custNo_" + data.data.allocateSeatType.uuid).html(data.data.custId);
+				var newDate = new Date();
+				newDate.setTime(data.data.createDate);
+				$("#createDate_" + data.data.allocateSeatType.uuid).html(newDate.format("yyyy-MM-dd h:m:ss"));
+				var statusText = "";
+				if(data.data.recodeStatus == "0"){
+					statusText = "0:可取号";
+				}else if(data.data.recodeStatus == "1"){
+					statusText = "1:已取号";
+				}else{
+					statusText = data.data.recodeStatus + ":已过号";
+				}
+				$("#status_" + data.data.allocateSeatType.uuid).html(statusText);
+				$(".alert-success").html(data.msg);
+				$(".alert-success").show();
+			}else{
+				$(".alert-danger").html(data.msg);
+				$(".alert-danger").show();
+			}
+			newUuid = data.data.uuid;
+			newNum = data.data.allocateNo;
+		},
+		error : function(xhr, status){
+			$(".alert-danger").html("请求异常");
+			$(".alert-danger").show();
+		},
+		complete : function( xhr, status ) {
+			timer = setTimeout(function() {
+				$(btn).removeAttr("disabled");
+			},1000);
+			
 		}
 	});
-	
-	
-	/**
-	 * 派号界面初始化
-	 */
-	function pageInit(data){
-		if(data==null || data.length == 0){
-			Ext.MessageBox.alert("错误","尚未设置派号规则，无法初始化出号功能！");
-			return;
-		}else{
-			var size = data.length;
-			if(size >= 4){
-//				viewPanel.
+}
+
+/**
+ * 出号方法
+ * @param typeId
+ * @returns
+ */
+function createNewNum(typeId, numUuid){
+	$("#btn_" + typeId).attr("disabled", "disabled");
+	var newUuid = "";
+	var newNum = "";
+	$.ajax({
+		url : ctx + "/allocateSeatNumberRecord/createNo.do",
+		data : {
+			typeId:typeId
+		},
+		type : "POST",
+		dataType : "json",
+		success : function(data){
+			if(data.success == true){
+				$("#num_" + typeId).html(data.data.num);
+				$("#custNo_" + typeId).html(data.data.custNo);
+				$("#createDate_" + typeId).html(data.data.createDate);
+				var statusText = "";
+				if(data.data.status == "0"){
+					statusText = "0:可取号";
+				}else if(data.data.status == "1"){
+					statusText = "0:已取号";
+				}else{
+					statusText = data.data.status + ":已过号";
+				}
+				$("#status_" + typeId).html(statusText);
+				$(".alert-success").html(data.msg);
+				$(".alert-success").show();
+			}else{
+				$(".alert-danger").html(data.msg);
+				$(".alert-danger").show();
 			}
-			for(var i = 0; i < data.length; i++){
-				var numberObj = data[i];
-//				viewPanel
-			}
+			newUuid = data.data.numUuid;
+			newNum = data.data.num;
+		},
+		error : function(xhr, status){
+			$(".alert-danger").html("请求异常");
+			$(".alert-danger").show();
+		},
+		complete : function( xhr, status ) {
+			timer = setTimeout(function() {
+				$("#btn_" + typeId).removeAttr("disabled");
+			},1000);
+			$("#btn_" + numUuid).attr("onclick", "invalidNum('" + newUuid + "', '" + newNum + "', this)");
 		}
-	}
-	
-});
+		
+	})
+}
+
+Date.prototype.format = function(format) {
+    var date = {
+           "M+": this.getMonth() + 1,
+           "d+": this.getDate(),
+           "h+": this.getHours(),
+           "m+": this.getMinutes(),
+           "s+": this.getSeconds(),
+           "q+": Math.floor((this.getMonth() + 3) / 3),
+           "S+": this.getMilliseconds()
+    };
+    if (/(y+)/i.test(format)) {
+           format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+    }
+    for (var k in date) {
+           if (new RegExp("(" + k + ")").test(format)) {
+                  format = format.replace(RegExp.$1, RegExp.$1.length == 1
+                         ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
+           }
+    }
+    return format;
+}
