9764aa8178c8a6079463ad4666274ae2d07f70d2
gmc870223 <gmc870223@localhost>
2017-12-07 11:35:22 +0800

    取号功能

--- TSSA/src/com/tssa/remote/custmer/CustomerGetNoController.java
+++ TSSA/src/com/tssa/remote/custmer/CustomerGetNoController.java
@@ -23,8 +23,10 @@ import com.tssa.allocateSeats.service.AllocateSeatNumberRecordService;
 import com.tssa.allocateSeats.service.AllocateSeatTypeSetService;
 import com.tssa.common.mode.DetachedCriteriaTS;
 import com.tssa.common.util.DateWarpUtils;
+import com.tssa.cooperationBusiness.service.CooperationService;
 import com.tssa.remote.common.BaseController;
 import com.tssa.remote.object.CustNumberVo;
+import com.tssa.remote.object.CustTakeNumberVO;
 
 /**
  * @author gmc870223
@@ -33,37 +35,39 @@ import com.tssa.remote.object.CustNumberVo;
 
 @Controller
 @RequestMapping("/clientGetNo")
-public class CustomerGetNoController extends BaseController{
-	
+public class CustomerGetNoController extends BaseController {
+
 	private static final Logger logger = LoggerFactory.getLogger(CustomerGetNoController.class);
-	
+
 	@Autowired
-	private AllocateSeatTypeSetService allocateSeatTypeSetService; 
+	private AllocateSeatTypeSetService allocateSeatTypeSetService;
 	@Autowired
 	private AllocateSeatNumberRecordService allocateSeatNumberRecordService;
+	@Autowired
+	private CooperationService cooperationService;
 
 	/**
-	 * 查询商户当前号数与排队等待人数
-	 * 请求方式：
+	 * 查询商户当前号数与排队等待人数 请求方式：
 	 * http://localhost:8080/TSSA/clientGetNo/showNum.do?vendorCode=xx
+	 * 
 	 * @param vendorCode
 	 * @param typeId
 	 * @return
 	 */
 	@RequestMapping("/showNum")
 	@ResponseBody
-	public Map<String, Object> showNum(String vendorCode){
-//		1、查出商户当前不同出号规则所出号码
-//		2、计算出不同出号规则所有记录状态等于1的记录进行统计，作为前面等待人数输出显示
+	public Map<String, Object> showNum(String vendorCode) {
+		// 1、查出商户当前不同出号规则所出号码
+		// 2、计算出不同出号规则所有记录状态等于1的记录进行统计，作为前面等待人数输出显示
 		Map<String, Object> result = new HashMap<String, Object>();
-		if(StringUtils.isEmpty(vendorCode)) {
+		if (StringUtils.isEmpty(vendorCode)) {
 			result.put("success", false);
 			result.put("msg", "商户未启用手机取号功能");
 			return result;
-		}else {
+		} else {
 			try {
 				List<CustNumberVo> list = allocateSeatNumberRecordService.getVendorAllNumberCount(vendorCode);
-				if(list != null && list.size() > 0 ) {
+				if (list != null && list.size() > 0) {
 					result.put("success", true);
 					result.put("msg", "");
 					result.put("data", list);
@@ -77,11 +81,11 @@ public class CustomerGetNoController extends BaseController{
 		}
 		return result;
 	}
-	
+
 	/**
-	 * 手机端出号功能
-	 * 请求方式：
+	 * 手机端出号功能 请求方式：
 	 * http://localhost:8080/TSSA/clientGetNo/extraNum.do?vendorCode=xx&custId=xx
+	 * 
 	 * @param vendorCode
 	 * @param typeId
 	 * @param vendorCode
@@ -89,56 +93,62 @@ public class CustomerGetNoController extends BaseController{
 	 */
 	@RequestMapping("/extraNum")
 	@ResponseBody
-	public Map<String, Object> extraNum(String typeId, String custId, String vendorCode){
-		
+	public Map<String, Object> extraNum(String typeId, String custId, String vendorCode) {
+
 		Map<String, Object> result = new HashMap<String, Object>();
 		try {
 			allocateSeatNumberRecordService.extraNum(typeId, custId);
 			List<CustNumberVo> list = allocateSeatNumberRecordService.getVendorAllNumberCount(vendorCode);
-			if(list != null && list.size() > 0 ) {
+			if (list != null && list.size() > 0) {
 				result.put("success", true);
 				result.put("msg", "");
 				result.put("data", list);
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			result.put("success", false);
 			result.put("msg", "获取商户等待人数失败");
 			logger.error(e.getMessage(), e);
 			e.printStackTrace();
 		}
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * 请求方式：
 	 * http://localhost:8080/TSSA/clientGetNo/checkCustHasTakeNumber.do?vendorCode=xx&custId=xx&typeId=xx
 	 * 检查用户是否已经取号
-	 * @param custId 客户编码
-	 * @param typeId 派号类型主键
+	 * 
+	 * @param custId
+	 *            客户编码
+	 * @param typeId
+	 *            派号类型主键
 	 * @return
 	 */
 	@RequestMapping("/checkCustHasTakeNumber")
 	@ResponseBody
-	public Map<String, Object> checkCustHasTakeNumber(String custId, String typeId){
+	public Map<String, Object> checkCustHasTakeNumber(String custId, String typeId) {
 		Map<String, Object> result = new HashMap<String, Object>();
 		try {
 			AllocateSeatTypeSet set = allocateSeatTypeSetService.get(AllocateSeatTypeSet.class, typeId);
-			if(set != null) {
-				DetachedCriteriaTS<AllocateSeatNumberRecord> detachedCriteria = new DetachedCriteriaTS<AllocateSeatNumberRecord>(AllocateSeatNumberRecord.class);
+			if (set != null) {
+				DetachedCriteriaTS<AllocateSeatNumberRecord> detachedCriteria = new DetachedCriteriaTS<AllocateSeatNumberRecord>(
+						AllocateSeatNumberRecord.class);
 				detachedCriteria.add(Restrictions.eq("allocateSeatType", set));
 				detachedCriteria.add(Restrictions.eq("custId", custId));
 				detachedCriteria.add(Restrictions.eq("recodeStatus", "1"));
-				detachedCriteria.add(Restrictions.between("createDate", DateWarpUtils.parseDate(DateWarpUtils.format(new Date()) + " 00:00:00"), DateWarpUtils.parseDate(DateWarpUtils.format(new Date()) + " 23:59:59")));
+				detachedCriteria.add(Restrictions.between("createDate",
+						DateWarpUtils.parseDate(DateWarpUtils.format(new Date()) + " 00:00:00"),
+						DateWarpUtils.parseDate(DateWarpUtils.format(new Date()) + " 23:59:59")));
 				AllocateSeatNumberRecord record = allocateSeatNumberRecordService.find(detachedCriteria);
-				if(record != null && StringUtils.isNotEmpty(record.getCustId())) {
+				if (record != null && StringUtils.isNotEmpty(record.getCustId())) {
 					result.put("success", true);
 					result.put("msg", "您已取号,请勿重复取号");
-					result.put("flag", "1");//向前端返回1表示客户已经取过号。前端应判断为不能取号
+					result.put("flag", "1");// 向前端返回1表示客户已经取过号。前端应判断为不能取号
 				} else {
 					result.put("success", true);
 					result.put("msg", "您已取号,请勿重复取号");
-					result.put("flag", "0");//向前端返回0表示客户未曾取号。前端应判断为可取号
+					result.put("flag", "0");// 向前端返回0表示客户未曾取号。前端应判断为可取号
 				}
 			}
 		} catch (Exception e) {
@@ -146,6 +156,46 @@ public class CustomerGetNoController extends BaseController{
 			result.put("success", false);
 			result.put("msg", "检查时发生异常");
 		}
+		return result;
+	}
+
+	/**
+	 * 请求方式：
+	 * http://localhost:8080/TSSA/clientGetNo/getCustTakeNumDetail.do?custId=xx&start=0&limit=10
+	 * 获取用户所有取号数据
+	 * 
+	 * @param custId
+	 * @return
+	 */
+	@RequestMapping("getCustTakeNumDetail")
+	@ResponseBody
+	public Map<String, Object> getCustTakeNumDetail(String custId, Integer start, Integer limit) {
+		Map<String, Object> result = new HashMap<String, Object>();
+
+		if (StringUtils.isEmpty(custId)) {
+			result.put("success", false);
+			result.put("msg", "参数有误");
+			return result;
+		}
+
+		try {
+			List<CustTakeNumberVO> resultData = allocateSeatNumberRecordService.getCustTakeNumberDeatil(custId, start, limit);
+			if(resultData != null && resultData.size() > 0) {
+				result.put("success", true);
+				result.put("data", resultData);
+				result.put("msg", "");
+			}else {
+				result.put("success", false);
+				result.put("data", null);
+				result.put("msg", "没有任何记录");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.put("success", false);
+			result.put("data", null);
+			result.put("msg", "查询异常");
+		}
+
 		return result;
 	}
 
